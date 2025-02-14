c70dfa971f405e1fbf4011d515e528d0970a0133
Yaroslav Ivanchenko <126415347+shagid76@users.noreply.github.com>
2025-02-14 06:10:00 -0500

    Update

--- Practice 1/task1.c
+++ Practice 1/task1.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 void secondsToTime(int seconds){
+      int inputValue = seconds; 
       int hours = seconds / 3600;
       seconds %= 3600;
       int minutes = seconds / 60;
       seconds %= 60;
-      printf("%d hours %d minutes %d seconds", hours, minutes, seconds);
+      if (hours < 0 || minutes < 0 || seconds < 0) {
+        printf("%d секунд еквівалентно %d годинам %d хвилинам %d секундам.\n", inputValue, hours, minutes, seconds);
+    } else {
+        printf("%d секунд еквівалентно %d годинам %d хвилинам %d секундам.\n", inputValue, hours, minutes, seconds);
+    }
     }
 
 int main(){
-    int inputSeconds;
-    printf("Input seconds number ");
-    scanf("%d", &inputSeconds);
-    secondsToTime(inputSeconds);
+    int seconds;
+    printf("Введіть кількість секунд ");
+    scanf("%d", &seconds);
+    secondsToTime(seconds);
     return 0;
 }
-
