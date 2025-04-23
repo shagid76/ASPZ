e9bcae2d623a2c1db492f04fbe6c3545d5e95ba0
shagid76 <igrish76@gmail.com>
2025-04-23 13:57:49 -0400

    Practice 6

---
+++ Practice 6/task.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};  
+    printf("Значення за індексом 5: %d\n", arr[5]); 
+    printf("Значення за індексом 6: %d\n", arr[6]); 
+    return 0;
+}
